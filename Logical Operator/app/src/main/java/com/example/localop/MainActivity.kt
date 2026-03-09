b7d0a3f2f9b5fe7c90ab3b71099ca2e2aebf0afe
chr1shouston <st10536381@myemeris.edu.za>
2026-03-09 08:29:00 +0200

    Declaring Variables
    Declaring Variables

--- Logical Operator/app/src/main/java/com/example/localop/MainActivity.kt
+++ Logical Operator/app/src/main/java/com/example/localop/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.localop
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
+
+    // Declaring variables
+    private lateinit var cbITStudent: CheckBox
+    private lateinit var edtName:EditText
+    private lateinit var edtAge: EditText
+    private lateinit var btnCheck: Button
+    private lateinit var cbStaff: CheckBox
+    private lateinit var cbBanned: CheckBox
+    private lateinit var tvResult: TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
